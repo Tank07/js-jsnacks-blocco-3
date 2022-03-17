90d8949a9cc57e0702bcb4202701991d84c23a29
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-17 15:09:29 +0100

    Prova push

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,6 +1,6 @@
 // Snack1
 // Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e
-// ad inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+// ad inserirli nell’array fino a quando la somma degli elementi è minore di 50. dadaddada
 
 let arrayVuoto = [];
 
