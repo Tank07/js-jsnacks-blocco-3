8560db8302dd9b6460bcfc2c1bfcaeb68d492e57
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-17 15:02:10 +0100

    Snack 1 completato

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,4 +1,22 @@
 // Snack1
-// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e
+// ad inserirli nell’array fino a quando la somma degli elementi è minore di 50.
 
+let arrayVuoto = [];
 
+let somma = 0;
+
+i=0;
+
+while (somma < 50) {
+    let numeroIns = parseInt(prompt("Inserisci un numero"));
+    arrayVuoto.push(numeroIns);
+    console.log(numeroIns)
+    somma += numeroIns;
+    console.log(somma);
+    i++;
+    
+}
+    console.log(arrayVuoto)
+    document.getElementById("esito").innerHTML = `<div>Dopo aver inserito "${i}" numeri, la somma è superiore a 50 ed è <span class="blue">"${somma}"</span> </div>`;    
+     
