9b300fd71c4f9f6c0aef19b5d1eb3f30e29556c8
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-17 22:17:03 +0100

    Prova push

--- snack-4/main.js
+++ snack-4/main.js
@@ -1,3 +1,34 @@
 // Snack4 (Bonus)
-// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa. Interrompi il ciclo appena il nome è stato trovato.
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
+//chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa. Interrompi il ciclo appena il nome è stato trovato.
+
+let nomi = ["Modestino", "Abdelhamid" ,"Andrea","Alberto","Vincenzo","Gerardo" ,"Luca","Tancredi","Valentino","Ludovica","Davide","Nizar","Alessandro","Gabriele" ,"Stefano","Angelo","Caterina","Sergio","Clara","Matteo","Simone" ,"Noemi","Roberto","Andrea","Andrea","Simone" ,"Samuele" ,"Vera","Ivan","Michael","Simone Luca","Francesco"];
+
+let cognomi = ["Arpa","Ayadi","Bellucci","Biga","Borriello","Calia","Carbone","Carluccio","De Stefano","Di Benedetto","Di Pietro","Fergag","Fonzi","Magni","Maiorchini","Melotti","Moccia","Nifosi","Nocilla","Nuvoloni","Pavone","Pintaldi","Piva","Pontillo","Rosellini","Saputo","Spadaro","Sprinceana","Toselli","Wachnicki","Zaino","Natoli"];
+
+// Creazione Lista nomi
+
+let lista = []
+
+let i = 0;
+
+while (i < nomi.length){
+    
+    let listNomi = nomi[i] + " " + cognomi[i];
+    console.log(listNomi);
+    i++;
+    lista.push(listNomi);
+}
+
+console.log(lista)
+
+// Controllo Lista
+
+let nomeImbucato = prompt("Buonasera signore può gentilmente dirmi il suo nome ? ");
+
+l=0;
+
+while (l < lista.length) {
+    
+}
 
