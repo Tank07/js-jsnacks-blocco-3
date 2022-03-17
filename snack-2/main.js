115fe8db9ef1874e14d6d87e54a2586b068b3475
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-17 15:36:10 +0100

    Bozza snack 2

--- snack-2/main.js
+++ snack-2/main.js
@@ -1,22 +1,17 @@
 // Snack2
-// Fai inserire un numero, che chiameremo N, all’utente. Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. Ogni volta che ne crei uno, stampalo.ù
+// Fai inserire un numero, che chiameremo N, all’utente. Genera N array, ognuno formato da 10 numeri casuali da 1 a 100.
+// Ogni volta che ne crei uno, stampalo.
 
 
+let numeroCreatore = parseInt(prompt("Quanti array vuoi creare ?"));
 
-// Snack2
-// Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
+let arrayUno = [];
 
+i=0;
 
-// let nomi = ["Modestino", "Abdelhamid" ,"Andrea","Alberto","Vincenzo","Gerardo" ,"Luca","Tancredi","Valentino","Ludovica","Davide","Nizar","Alessandro","Gabriele" ,"Stefano","Angelo","Caterina","Sergio","Clara","Matteo","Simone" ,"Noemi","Roberto","Andrea","Andrea","Simone" ,"Samuele" ,"Vera","Ivan","Michael","Simone Luca","Francesco"];
-// console.log(nomi.length)
+while (i<10) {
+    arrayUno.push(Math.floor(Math.random()*100));
+    i++;
+}
 
-// let cognomi = ["Arpa","Ayadi","Bellucci","Biga","Borriello","Calia","Carbone","Carluccio","De Stefano","Di Benedetto","Di Pietro","Fergag","Fonzi","Magni","Maiorchini","Melotti","Moccia","Nifosi","Nocilla","Nuvoloni","Pavone","Pintaldi","Piva","Pontillo","Rosellini","Saputo","Spadaro","Sprinceana","Toselli","Wachnicki","Zaino","Natoli"]
-// console.log(cognomi.length)
-
-// for (i=1; i < nomi.length; i++){
-//     let randomNome = Math.floor(Math.random()*nomi.length);
-//     let randomCognome = Math.floor(Math.random()*cognomi.length);
-//     document.getElementById("esito").innerHTML += `<div> <span class="white">L'invitato numero ${i} per l'esclusiva festa dei Booleani in casa Gatsby è </span> <span class="blue">"${nomi[randomNome]} ${cognomi[randomCognome]}"</span></div>`;
-
-// }
-
+console.log(arrayUno);
